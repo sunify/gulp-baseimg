9477da7d7707c2dd90dcc6eebb72fddcf7a1753c
Alexander Lunyov <isuntc@gmail.com>
2014-03-28 00:44:25 +0900

    Bug with wrong width and height

--- index.js
+++ index.js
@@ -7,28 +7,34 @@ var gutil = require('gulp-util');
 var mime = require('mime');
 
 module.exports = function(opts) {
-  var tpl = fs.readFileSync(opts.styleTemplate).toString();
-  var buffer = [];
-  var parser = new imgParser();
+    var tpl = fs.readFileSync(opts.styleTemplate).toString();
+    var buffer = [];
 
-  var bufferContents = function(file) {
-    var imgData;
-    if(parser.parse(file.contents) === imgParser.DONE) {
-      imgData = parser.getResult();
-      imgData.data = 'url(data:' + mime.lookup(imgData.format) + ';base64,' + file.contents.toString('base64') + ')';
-      imgData.name = path.basename(file.path, '.' + imgData.format);
-      buffer.push(imgData);
-    }
-  };
+    var bufferContents = function(file) {
+        var parser = new imgParser(),
+            imgData;
+        if(parser.parse(file.contents) === imgParser.DONE) {
+            imgData = parser.getResult();
+            imgData.data = [
+                'url(data:',
+                mime.lookup(imgData.format),
+                ';base64,',
+                file.contents.toString('base64'),
+                ')'
+            ].join('');
+            imgData.name = path.basename(file.path, '.' + imgData.format);
+            buffer.push(imgData);
+        }
+    };
 
-  var endStream = function() {
-    this.emit('data', new gutil.File({
-      contents: new Buffer(mustache.render(tpl, {
-        items: buffer
-      })),
-      path: opts.styleName
-    }));
-  };
+    var endStream = function() {
+        this.emit('data', new gutil.File({
+            contents: new Buffer(mustache.render(tpl, {
+                items: buffer
+            }), 'utf8'),
+            path: opts.styleName
+        }));
+    };
 
-  return new through(bufferContents, endStream);
+    return new through(bufferContents, endStream);
 };
