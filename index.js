300305cee99f0b6e6878c1da34ee1597cd15a58e
Alexander Lunyov <isuntc@gmail.com>
2014-03-27 18:17:31 +0900

    Build css by images

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
 var fs = require('fs');
 var path = require('path');
-var sizeOf = require('image-size');
+var imgParser = require('imagesize').Parser;
 var mustache = require('mustache');
+var through = require('through');
+var gutil = require('gulp-util');
+var mime = require('mime');
 
 module.exports = function(opts) {
-  
+  var tpl = fs.readFileSync(opts.styleTemplate).toString();
+  var buffer = [];
+  var parser = new imgParser();
+
+  var bufferContents = function(file) {
+    var imgData;
+    if(parser.parse(file.contents) === imgParser.DONE) {
+      imgData = parser.getResult();
+      imgData.data = 'url(data:' + mime.lookup(imgData.format) + ';base64,' + file.contents.toString('base64') + ')';
+      imgData.name = path.basename(file.path, '.' + imgData.format);
+      buffer.push(imgData);
+    }
+  };
+
+  var endStream = function() {
+    this.emit('data', new gutil.File({
+      contents: new Buffer(mustache.render(tpl, {
+        items: buffer
+      }))
+    }));
+  };
+
+  return new through(bufferContents, endStream);
 };
