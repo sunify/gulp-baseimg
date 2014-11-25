8d308c5830ca2e78780eaae52ab9353c51001309
Александр Лунев <isuntc@gmail.com>
2014-11-25 18:16:32 +0800

    Add svg support

--- index.js
+++ index.js
@@ -5,37 +5,57 @@ var mustache = require('mustache');
 var through = require('through');
 var gutil = require('gulp-util');
 var mime = require('mime');
+var SVGO = require('svgo');
+var svgo = new SVGO();
 
 module.exports = function(opts) {
-    var tpl = fs.readFileSync(opts.styleTemplate).toString();
-    var buffer = [];
+	var tpl = fs.readFileSync(opts.styleTemplate).toString();
+	var buffer = [];
+	var svgs = [];
 
-    var bufferContents = function(file) {
-        var parser = new imgParser(),
-            imgData;
-        if(parser.parse(file.contents) === imgParser.DONE) {
-            imgData = parser.getResult();
-            imgData.data = [
-                'url(data:',
-                mime.lookup(imgData.format),
-                ';base64,',
-                file.contents.toString('base64'),
-                ')'
-            ].join('');
-            imgData.name = path.basename(file.path, '.' + imgData.format);
-            buffer.push(imgData);
-        }
-    };
+	var bufferContents = function(file) {
+		var parser = new imgParser();
+		var type = mime.lookup(file.path);
+		var imgData;
 
-    var endStream = function() {
-        this.emit('data', new gutil.File({
-              contents: new Buffer(mustache.render(tpl, {
-                  items: buffer
-              }), 'utf8'),
-              path: opts.styleName
-          }));
-        this.emit('end');
-    };
+		if(type !== 'image/svg+xml' && parser.parse(file.contents) === imgParser.DONE) {
+			imgData = parser.getResult();
+			imgData.data = [
+				'url(data:',
+				mime.lookup(imgData.format),
+				';base64,',
+				file.contents.toString('base64'),
+				')'
+			].join('');
+			imgData.name = path.basename(file.path, '.' + imgData.format);
+			buffer.push(imgData);
+		} else if(type === 'image/svg+xml') {
+			svgo.optimize(file.contents.toString(), function(res) {
+				imgData = {
+					width: res.info.width,
+					height: res.info.height,
+					data: [
+						'url(data:image/svg+xml;utf8,',
+						encodeURIComponent(res.data),
+						')'
+					].join(''),
+					format: 'svg',
+					name: path.basename(file.path, '.svg')
+				};
+				buffer.push(imgData);
+			});
+		}
+	};
 
-    return new through(bufferContents, endStream);
+	var endStream = function() {
+		this.emit('data', new gutil.File({
+			  contents: new Buffer(mustache.render(tpl, {
+				  items: buffer
+			  }), 'utf8'),
+			  path: opts.styleName
+		  }));
+		this.emit('end');
+	};
+
+	return new through(bufferContents, endStream);
 };
--- test/main.js
+++ test/main.js
@@ -6,59 +6,59 @@ var postcss = require('postcss');
 require('mocha');
 
 var loadFile = function(path) {
-  return new gutil.File({
-    path: path,
-    contents: fs.readFileSync(path)
-  });
+	return new gutil.File({
+		path: path,
+		contents: fs.readFileSync(path)
+	});
 };
 
 var testCssSelector = function(rule, expect) {
-  return rule._selector === expect;
+	return rule._selector === expect;
 };
 
 describe('gulp-baseimg', function() {
 
-  describe('baseimg()', function() {
-    it('should compile couple images in signle stylesheet', function() {
-      var stream = baseimg({
-        styleTemplate: './test/fixtures/tmp.css.mustache'
-      });
-      var img1 = loadFile('./test/fixtures/icon1.png');
-      var img2 = loadFile('./test/fixtures/icon2.png');
+	describe('baseimg()', function() {
+		it('should compile three images in signle stylesheet', function() {
+			var stream = baseimg({
+				styleTemplate: './test/fixtures/tmp.css.mustache'
+			});
+			var img1 = loadFile('./test/fixtures/icon1.png');
+			var img2 = loadFile('./test/fixtures/icon2.png');
+			var img3 = loadFile('./test/fixtures/icon3.svg');
+			var sizes = [
+				{width: '18px', height: '15px'},
+				{width: '18px', height: '15px'},
+				{width: '14px', height: '13px'},
+			];
 
-      stream.on('data', function(newFile) {
-        var css = postcss.parse(newFile.contents.toString());
+			stream.on('data', function(newFile) {
+				var css = postcss.parse(newFile.contents.toString());
 
-        describe('css', function() {
+				describe('css', function() {
+					it('should have right selectors', function() {
+						for(var i = 0; i < 3; i += 1) {
+							should(testCssSelector(css.rules[i], '.icon' + (i + 1))).be.ok;
+						}
+					});
 
-          it('should have right selectors', function() {
-            should(testCssSelector(css.rules[0], '.icon1')).be.ok;
-            should(testCssSelector(css.rules[1], '.icon2')).be.ok;
-          });
+					it('should have right width and height', function() {
+						css.rules[0].decls.map(function(item, i) {
+							if(sizes[i][item.prop]) {
+								item._value.should.be.exactly(sizes[i][item.prop]);
+							}
+						});
+					});
 
-          it('should have right width and height', function() {
+				});
 
-            css.rules[0].decls.map(function(item) {
-              switch(item.prop) {
-                case 'width':
-                  item._value.should.be.exactly('18px');
-                  break;
+			});
 
-                case 'height':
-                  item._value.should.be.exactly('15px');
-                  break;
-              }
-            });
-          });
-
-        });
-
-      });
-
-      stream.write(img1);
-      stream.write(img2);
-      stream.end();
-    });
-  });
+			stream.write(img1);
+			stream.write(img2);
+			stream.write(img3);
+			stream.end();
+		});
+	});
 
 });
